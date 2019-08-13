183788672c7f55f524e7bc1f9c651a766253043f
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-13 18:07:22 +0200

    Added snapping to the cubes based on the grid and the number on each cube also based on the grid.

---
+++ Medieval Defense/Assets/CubeEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+
+    [SerializeField] [Range(1f, 20f)] private float gridSize = 10f;
+    private TextMesh textMesh;
+
+    private void Update()
+    {
+        Vector3 snapPosition;
+
+        gridSize = Mathf.RoundToInt(gridSize);
+        snapPosition.x = Mathf.RoundToInt(transform.position.x/ gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z/ gridSize) * gridSize;
+        transform.position=new Vector3(snapPosition.x,0f,snapPosition.z);
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPosition.x/gridSize + ","+snapPosition.z/gridSize;
+    }
+}
