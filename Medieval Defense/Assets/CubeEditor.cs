a20604770bc702c9795ae4a10728562f9efe6606
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-13 20:09:22 +0200

    Added simple path walking system

--- Medieval Defense/Assets/CubeEditor.cs
+++ Medieval Defense/Assets/CubeEditor.cs
@@ -20,6 +20,8 @@ public class CubeEditor : MonoBehaviour
         snapPosition.z = Mathf.RoundToInt(transform.position.z/ gridSize) * gridSize;
         transform.position=new Vector3(snapPosition.x,0f,snapPosition.z);
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPosition.x/gridSize + ","+snapPosition.z/gridSize;
+        string labelText = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
+        gameObject.name ="Cube "+ labelText;
+        textMesh.text = labelText;
     }
 }
---
+++ Medieval Defense/Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private List<Waypoint> path;
+
+    private void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        Debug.Log("Starting Patrol...");
+        foreach (var node in path)
+        {
+            Debug.Log("Visiting "+node.name);
+            transform.position = node.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("Ending Patrol");
+    }
+}
