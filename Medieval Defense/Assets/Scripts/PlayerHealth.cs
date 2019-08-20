39486799d1bfc051108942a39fcc9286698b4fcf
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-20 12:30:37 +0200

    Added basic health update for player

---
+++ Medieval Defense/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int health = 10;
+    [SerializeField] private int healthDecrease = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8) // Enemy layer is number 8
+        {
+            DecreaseBaseHealth();
+        }
+    }
+
+    private void DecreaseBaseHealth()
+    {
+        health -= healthDecrease;
+    }
+}
